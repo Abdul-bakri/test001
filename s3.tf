76ef65dfc88594608800d30c00fc9a3d89a1a615
Abubakri Olayide Abdul-lateef <66844942+Abdul-bakri@users.noreply.github.com>
2022-06-29 20:55:04 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "central-logging-us-east-1-today" {
+resource "aws_s3_bucket" "central-logging-${data.aws_region.current.name}-today" {
   force_destroy = true
   bucket        = "central-logging-${data.aws_region.current.name}-today"
   acl           = "private"
