2612d8dbee93e3322dcaca2bee7642427676da7e
Abubakri Olayide Abdul-lateef <66844942+Abdul-bakri@users.noreply.github.com>
2022-07-10 09:41:24 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZ2GBMUBB2ZXOQSN4"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   profile = "accountDestination"
   }
