1cfd6616d1cfb66e5ee3e5057b96eb802e6bbbf7
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-11 10:56:34 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "central-logging-us-east-1-today" {
   force_destroy = true
-  bucket        = "central-logging-${data.aws_region.current.name}-today-poland"
+  bucket        = "central-logging-${data.aws_region.current.name}-today-nigeria"
   acl           = "private"
   
   versioning {
