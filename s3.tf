f3384a0458813cb94916aa6a332d2ac301691f4f
Pr3c10us <96595416+Pr3c10us@users.noreply.github.com>
2022-07-11 10:26:38 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "central-logging-us-east-1-today" {
   force_destroy = true
   bucket        = "central-logging-${data.aws_region.current.name}-today-poland"
   acl           = "private"
-  region        = "us-east-1"
+#   region        = "us-east-1"
 
   versioning {
     enabled = true
